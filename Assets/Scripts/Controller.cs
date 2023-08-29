ce83fe9789a8605840dece89e40d67cca250d41f
Siem <siemgdeveloper@gmail.com>
2023-08-29 12:12:18 -0400

    Added ControllerManager
    Added Input, Controller, and ControllerManager scripts

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,35 @@ public class Controller : MonoBehaviour
 
     //What button was actually pressed
 
+    public int Index { get; private set; }
+    public bool IsAssigned { get; set; }
 
+    private string attackButton;
 
+    public bool attack;
+
+    private void Start()
+    {
+        if(!string.IsNullOrEmpty(attackButton))
+        {
+            attackButton = "Attack" + Index;
+        }
+    }
+
+    private void Update()
+    {
+        attack = Input.GetButton(attackButton);
+    }
+
+    internal void SetIndex(int index)
+    {
+        Index = index;
+        attackButton = "Attack" + Index;
+        gameObject.name = "Controller" + Index;
+    }
+
+    internal bool AnyButtonDown()
+    {
+        return attack;
+    }
 }
---
+++ Assets/Scripts/ControllerManager.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ControllerManager : MonoBehaviour
+{
+    private List<Controller> controllers;
+
+    private void Awake()
+    {
+        //Find all the controllers available
+        controllers = FindObjectsOfType<Controller>().ToList();
+
+        //Starting with the first, assign each controller a number (so Controller 1, Controller 2..3..4... etc.)
+        int index = 1;
+        foreach (var controller in controllers)
+        {
+            controller.SetIndex(index);
+            index++;
+        }
+    }
+
+    private void Update()
+    {
+        foreach (var controller in controllers)
+        {
+            if(controller.IsAssigned == false && controller.AnyButtonDown())
+            {
+                AsignControler(controller);
+            }
+        }
+    }
+
+    private void AsignControler(Controller controller)
+    {
+        controller.IsAssigned = true;
+        Debug.Log("Assigned Controller: " + controller.gameObject.name);
+    }
+}

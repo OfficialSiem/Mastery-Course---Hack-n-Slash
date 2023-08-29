a3b0c1e2154dbe3802324d04e3f02bd17db7e724
Siem <siemgdeveloper@gmail.com>
2023-08-29 11:15:56 -0400

    Initial commit

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    //What button does what attack/move
+
+    //What player is pressing button
+
+    //What button was actually pressed
+
+
+
+}
